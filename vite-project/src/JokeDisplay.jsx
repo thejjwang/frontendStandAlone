6fcd34c23a1ef832939e98efedc47dfbcfec96ad
thejjwang <jonathanjacobwang@gmail.com>
2023-09-21 17:05:03 -0700

    need to implement modal if id is not in array

--- vite-project/src/JokeDisplay.jsx
+++ vite-project/src/JokeDisplay.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const JokeDisplay = ({jokeById, randomJoke}) => {
+ // added jokeById.id && trying to handle if user inputted id isnt in the joke array
+ // needs work
   return (
     <div>
       <h3>{randomJoke.text}</h3>
-      <h3>{jokeById.text}</h3>
+      
+      <h3>{jokeById.id && jokeById.text}</h3>
     </div>
   )
 }
