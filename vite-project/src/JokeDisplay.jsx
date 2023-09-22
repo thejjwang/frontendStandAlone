21202f826dd41aa74eb51fd09d414ee17e803634
thejjwang <jonathanjacobwang@gmail.com>
2023-09-22 10:12:33 -0700

    jokedisplay styled

--- vite-project/src/JokeDisplay.jsx
+++ vite-project/src/JokeDisplay.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const JokeDisplay = ({jokeById, randomJoke}) => {
- // added jokeById.id && trying to handle if user inputted id isnt in the joke array
- // needs work
+const JokeDisplay = ({ jokeById, randomJoke }) => {
+  // added jokeById.id && trying to handle if user inputted id isnt in the joke array
+  // needs work
   return (
-    <div>
-      <h3>{randomJoke.text}</h3>
-      
-      <h3>{jokeById.id && jokeById.text}</h3>
-    </div>
-  )
-}
+    <div className="flex justify-center h-screen">
+      <div className="bg-white p-4 rounded-lg shadow-md mt-10 w-11/12 h-16">
+        <h3>{randomJoke.text}</h3>
 
-export default JokeDisplay
+        <h3>{jokeById.id && jokeById.text}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default JokeDisplay;
