4b73ece81d0d194bc6567cca02408c7c9b926943
thejjwang <jonathanjacobwang@gmail.com>
2023-09-23 14:37:20 -0700

    moved around input and button

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -55,7 +55,15 @@ function App() {
       <div className="gap-6 text-center">
         <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800" onClick={fetchRandomJoke}>
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md group-hover:bg-opacity-0">
-            Get Random Joke
+            Get A Random Joke
+          </span>
+        </button>
+        <button
+          className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+          onClick={() => fetchJokeById(userInput)}
+        >
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md group-hover:bg-opacity-0">
+            Get Joke By Id
           </span>
         </button>
         <input
@@ -66,14 +74,6 @@ function App() {
           onChange={(e) => setUserInput(e.target.value)}
           required
         ></input>
-        <button
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-          onClick={() => fetchJokeById(userInput)}
-        >
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md group-hover:bg-opacity-0">
-            Get Joke By Id
-          </span>
-        </button>
       </div> 
       {showJokeDisplay && <JokeDisplay jokeById={jokeById} randomJoke={randomJoke} />}
     </div>
