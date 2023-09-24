5608fcfd7065c5e2a2bf7601b72e18b74b2f5165
thejjwang <jonathanjacobwang@gmail.com>
2023-09-24 13:24:26 -0700

    fix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
           Bringing Laughter to Your Day
         </p>
       </div>
-      <div className="gap-6 text-center mt-20">
+      <div id="fix" className="gap-6 text-center mt-10">
         <button
           className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
           onClick={fetchRandomJoke}
@@ -73,7 +73,7 @@ function App() {
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md group-hover:bg-opacity-0">
             Get Joke By Id
           </span>
-        </button>
+        </button> 
         {requiredTextVisible && (
         <p style={{ color: "red" }}>no id found/incorrect input</p>
         )}
